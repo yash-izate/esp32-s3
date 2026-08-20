d9b2cd2f8e5fa0946d790f1ec9432095e62a8d4e
Yash Izate <yashizate.softech@gmail.com>
2026-08-20 19:14:54 +0530

    blink onboard led done

--- project_v1/main/main.c
+++ project_v1/main/main.c
@@ -1,16 +1,56 @@
 #include <stdio.h>
+
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "esp_log.h"
 
-static const char *TAG = "PROJECT_V1";
+#include "led_strip.h"
+#include "led_strip_rmt.h"
+
+#define LED_GPIO 48
+#define LED_COUNT 1
 
 void app_main(void)
 {
+    led_strip_handle_t led_strip;
+
+    led_strip_config_t strip_config = {
+        .strip_gpio_num = LED_GPIO,
+        .max_leds = LED_COUNT,
+    };
+
+    led_strip_rmt_config_t rmt_config = {
+        .resolution_hz = 10 * 1000 * 1000,
+        .flags.with_dma = false,
+    };
+
+    ESP_ERROR_CHECK(
+        led_strip_new_rmt_device(
+            &strip_config,
+            &rmt_config,
+            &led_strip
+        )
+    );
+
     while (1)
     {
-        ESP_LOGI(TAG, "Hello from ESP32-S3!");
+        // RED
+        led_strip_set_pixel(led_strip, 0, 255, 0, 0);
+        led_strip_refresh(led_strip);
+        vTaskDelay(pdMS_TO_TICKS(1000));
 
+        // GREEN
+        led_strip_set_pixel(led_strip, 0, 0, 255, 0);
+        led_strip_refresh(led_strip);
+        vTaskDelay(pdMS_TO_TICKS(1000));
+
+        // BLUE
+        led_strip_set_pixel(led_strip, 0, 0, 0, 255);
+        led_strip_refresh(led_strip);
+        vTaskDelay(pdMS_TO_TICKS(1000));
+
+        // OFF
+        led_strip_clear(led_strip);
+        led_strip_refresh(led_strip);
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
