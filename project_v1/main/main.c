3853673635ea0fe02a9879b9b43e443db2cc1c1e
Yash Izate <yashizate.softech@gmail.com>
2026-08-20 16:55:15 +0530

    setup done

--- project_v1/main/main.c
+++ project_v1/main/main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+
+static const char *TAG = "PROJECT_V1";
 
 void app_main(void)
 {
+    while (1)
+    {
+        ESP_LOGI(TAG, "Hello from ESP32-S3!");
 
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
 }
